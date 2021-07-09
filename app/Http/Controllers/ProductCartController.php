c63f6cb0c4682d7c60f2d67f8f9be4e6f75d9f34
Lisandro Caballero <lisandrocaballero13@gmail.com>
2021-07-09 12:37:10 -0300

    se creo la tabla pivot para la relacion de muchos a muchos y el carrito

---
+++ app/Http/Controllers/ProductCartController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
+use App\Services\CartServices;
+use Illuminate\Validation\ValidationException;
+
+
+class ProductCartController extends Controller
+{
+    public $cartService;
+    public function __construct(CartServices $cartService )
+    {
+        $this->cartService = $cartService;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Product $product)
+    {
+
+        $cart = $this->cartService->getFromCookieOrCreate();
+
+        $quantity = $cart->products()
+            ->find($product->id)
+            ->pivot
+            ->quantity ?? 0;
+        if ($product->stock < $quantity + 1) {
+            throw ValidationException::withMessages([
+                'product' => "There is not enough stock for the quantity you required of {$product->title}",
+            ]);
+        }
+
+        $cart->product()->syncWithoutDetaching([
+            $product->id => ['quantity' => $quantity + 1],
+        ]);
+
+        $cookie =  Cookie::make('cart', $cart->id, 7 * 24 * 60);
+        return redirect()->back()->cookie($cookie);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product, Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product, Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product, Cart $cart)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @param  \App\Models\Cart  $cart
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product, Cart $cart)
+    {
+        //
+    }
+
+
+
+
+}
